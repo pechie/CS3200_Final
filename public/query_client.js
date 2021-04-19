36adc2c812a20d9353fbb845406cb00f1d6359f4
Nick Pechie <pechie717@gmail.com>
2021-04-19 17:41:43 -0400

    Add ability to search for release by id

--- public/query_client.js
+++ public/query_client.js
@@ -3,7 +3,56 @@ homeButton.addEventListener('click', function(e) {
   window.location.href = "index.html";
 });
 
-const queryButton = document.getElementById("query_button");
-queryButton.addEventListener('click', function(e) {
-  window.location.href = "query.html";
+const searchButton1 = document.getElementById("search_button_1");
+searchButton1.addEventListener('click', function(e) {
+  let releaseId = document.getElementById("release_id").value;
+
+  const data = {
+    "release_id": releaseId
+  }
+
+  fetch('/search-1-clicked', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+  }).then(async function(response) {
+    let release_info = await response.json();
+      if (response.status && response.status == 404) {
+        document.getElementById("result").innerHTML = "Release not found, please try again";
+        return;
+      } else {
+        document.getElementById("result").innerHTML = "Found release: " + JSON.stringify(release_info[0]);
+        return;
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+
+const searchButton2 = document.getElementById("search_button_2");
+searchButton2.addEventListener('click', function(e) {
+  let releaseId = document.getElementById("release_id").value;
+
+  const data = {
+    "release_id": releaseId
+  }
+
+  fetch('/search-2-clicked', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+  }).then(function(response) {
+    console.log(response);
+      if (response.status && response.status == 404) {
+        document.getElementById("result").innerHTML = "Release not found, please try again";
+        return;
+      } else {
+        document.getElementById("result").innerHTML = "Should be working";
+        return;
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 });
--- server.js
+++ server.js
@@ -43,16 +43,47 @@ app.post('/table-clicked', (req, res) => {
 app.post('/log-in-clicked', (req, res) => {
   // connect to database
   con.connect(function(err) {
+    // query for given username and email
     con.query("SELECT * FROM users WHERE username = \'" + req.body.username + "\' AND email = \'" + req.body.email + "\'", 
-    function (err, result, fields) {
+    function (err, result) {
+      // send back error if no result is found
       if (err || result.length == 0) {
         res.sendStatus(404);
       } else {
         console.log(result);
+        // ok status
         res.sendStatus(200);
       }
     });
   });
-  // query for user
   console.dir(req.body, {depth: null});
+});
+
+app.post('/search-1-clicked', (req, res) => {
+  console.log("search 1 clicked");
+  con.connect(function(err) {
+    con.query("SELECT * FROM releases WHERE release_id = " + req.body.release_id, 
+    function (err, result) {
+      console.log(result);
+      if (err || result.length == 0) {
+        res.sendStatus(404);
+      } else {
+        res.status(200).send(JSON.stringify(result));
+      }
+    });
+  });
+});
+
+app.post('/search-2-clicked', (req, res) => {
+  con.connect(function(err) {
+    con.query("SELECT * FROM releases WHERE artist = \'" + req.body.artist + "\' AND title = \'" + req.body.title + "\'", 
+    function (err, result) {
+      if (err || result.length == 0) {
+        res.sendStatus(404);
+      } else {
+        res.send(result);
+        
+      }
+    });
+  });
 });
