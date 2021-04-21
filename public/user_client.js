99b25bef03261533173d9541c4b4abd064bf4ac5
Brad <bradebeckert@gmail.com>
2021-04-20 20:28:03 -0400

    Added functionality to create new users

--- public/user_client.js
+++ public/user_client.js
@@ -1,4 +1,40 @@
 const homeButton = document.getElementById("home_button");
-homeButton.addEventListener('click', function(e) {
+homeButton.addEventListener('click', function (e) {
   window.location.href = "index.html";
+});
+
+const addUser = document.getElementById('add_user');
+addUser.addEventListener('click', (e) => {
+  let username = document.getElementById('username').value;
+  let email = document.getElementById('email').value;
+  let bio = document.getElementById('bio').value;
+
+  const data = {
+    'username': username,
+    'email': email,
+    'bio': bio
+  }
+
+  fetch('/add-user', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  }).then(async function (response) {
+    if (response.status && response.status == 404) {
+      return;
+    } else {
+      return;
+    }
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
+
+$('#username').bind('keyup', function () {
+  if ($('#username').val() != '') {
+    $('#add_user').removeAttr('disabled');
+  } else {
+    $('#add_user').attr('disabled', 'disabled');
+  }
 });
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "password",
+  password: 'password',
   database: "rate_your_music"
 });
 
@@ -27,66 +27,97 @@ app.get('/', (req, res) => {
 
 app.post('/log-in-clicked', (req, res) => {
   // connect to database
-  con.connect(function(err) {
+  con.connect(function (err) {
     // query for given username and email
-    con.query("SELECT * FROM users WHERE username = \'" + req.body.username + "\' AND email = \'" + req.body.email + "\'", 
-    function (err, result) {
-      // send back error if no result is found
-      if (err || result.length == 0) {
-        res.sendStatus(404);
-      } else {
-        console.log(result);
-        // ok status
-        res.sendStatus(200);
-      }
-    });
+    con.query("SELECT * FROM users WHERE username = \'" + req.body.username + "\' AND email = \'" + req.body.email + "\'",
+      function (err, result) {
+        // send back error if no result is found
+        if (err || result.length == 0) {
+          res.sendStatus(404);
+          console.log('INVALID LOGIN')
+        } else {
+          console.log(result);
+          // ok status
+          res.sendStatus(200);
+        }
+      });
   });
-  console.dir(req.body, {depth: null});
+  console.dir(req.body, { depth: null });
 });
 
 app.post('/search-1-clicked', (req, res) => {
   console.log("search 1 clicked");
-  con.connect(function(err) {
-    con.query("SELECT * FROM releases WHERE release_id = " + req.body.release_id, 
-    function (err, result) {
-      console.log(result);
-      if (err || result.length == 0) {
-        res.sendStatus(404);
-      } else {
-        res.status(200).send(JSON.stringify(result));
-      }
-    });
+  con.connect(function (err) {
+    con.query("SELECT * FROM releases WHERE release_id = " + req.body.release_id,
+      function (err, result) {
+        console.log(result);
+        if (err || result.length == 0) {
+          res.sendStatus(404);
+        } else {
+          res.status(200).send(JSON.stringify(result));
+        }
+      });
   });
 });
 
 app.post('/search-2-clicked', (req, res) => {
-  con.connect(function(err) {
-    con.query("SELECT * FROM releases WHERE artist = \'" + req.body.artist + "\' AND title = \'" + req.body.title + "\'", 
-    function (err, result) {
-      console.log(result);
-      if (err || result.length == 0) {
-        res.sendStatus(404);
-      } else {
-        res.status(200).send(JSON.stringify(result));
-      }
-    });
+  con.connect(function (err) {
+    con.query("SELECT * FROM releases WHERE artist = \'" + req.body.artist + "\' AND title = \'" + req.body.title + "\'",
+      function (err, result) {
+        console.log(result);
+        if (err || result.length == 0) {
+          res.sendStatus(404);
+        } else {
+          res.status(200).send(JSON.stringify(result));
+        }
+      });
   });
 });
 
+app.post('/add-user', (req, res) => {
+  con.connect((err) => {
+    con.query(`INSERT INTO users `
+      + `VALUES('${req.body.username}', '${req.body.email}', '${req.body.bio}')`, (err, result) => {
+        if (err) {
+          console.log(err);
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(200);
+        }
+      })
+  })
+})
+
 app.post('/add-update-review', (req, res) => {
-  con.connect(function(err) {
+  con.connect(function (err) {
     con.query("SELECT * FROM reviews WHERE reviews.release = " + req.body.release_id + " AND reviews.user = '" + req.body.username + "'",
-    function(err, result) {
-      if (!result || result.length == 0) {
-        // add review
-        let newReleaseId = 0;
-        con.query("SELECT MAX(reviews_id) FROM reviews", function(err, result) {
-          if (!err) {
-            newReleaseId = result[0]['MAX(reviews_id)'] + 1;
-            con.query("INSERT INTO reviews "
-            + "values (" + newReleaseId + ",'" + req.body.username + "'," 
-            + req.body.release_id + "," + req.body.rating + ",'" + req.body.comments + "')",
-            function(err, result) {
+      function (err, result) {
+        if (!result || result.length == 0) {
+          // add review
+          let newReleaseId = 0;
+          con.query("SELECT MAX(reviews_id) FROM reviews", function (err, result) {
+            if (!err) {
+              newReleaseId = result[0]['MAX(reviews_id)'] + 1;
+              con.query("INSERT INTO reviews "
+                + "values (" + newReleaseId + ",'" + req.body.username + "',"
+                + req.body.release_id + "," + req.body.rating + ",'" + req.body.comments + "')",
+                function (err, result) {
+                  if (err) {
+                    console.log(err);
+                    res.sendStatus(404);
+                  } else {
+                    res.sendStatus(200);
+                  }
+                });
+            }
+          });
+        } else {
+          // update review
+          let reviewId = result[0].reviews_id;
+          con.query("UPDATE reviews SET reviews.rating = " + req.body.rating
+            + ", reviews.comments= '" + req.body.comments + "' "
+            + "WHERE reviews.reviews_id = " + reviewId,
+            function (err, result) {
               if (err) {
                 console.log(err);
                 res.sendStatus(404);
@@ -94,38 +125,22 @@ app.post('/add-update-review', (req, res) => {
                 res.sendStatus(200);
               }
             });
-          }
-        });
-      } else {
-        // update review
-        let reviewId = result[0].reviews_id;
-        con.query("UPDATE reviews SET reviews.rating = " + req.body.rating 
-        + ", reviews.comments= '" + req.body.comments + "' "
-        + "WHERE reviews.reviews_id = " + reviewId,
-        function(err, result) {
-          if (err) {
-            console.log(err);
-            res.sendStatus(404);
-          } else {
-            res.sendStatus(200);
-          }
-        });
-      }
-    });
+        }
+      });
   });
 });
 
 app.post('/delete-review', (req, res) => {
-  con.connect(function(err) {
-    con.query("DELETE FROM reviews WHERE reviews.user = '" 
-    + req.body.username + "' AND reviews.release = " + req.body.release_id, 
-    function (err, result) {
-      if (err) {
-        console.log(err);
-        res.sendStatus(404);
-      } else {
-        res.sendStatus(200);
-      }
-    });
+  con.connect(function (err) {
+    con.query("DELETE FROM reviews WHERE reviews.user = '"
+      + req.body.username + "' AND reviews.release = " + req.body.release_id,
+      function (err, result) {
+        if (err) {
+          console.log(err);
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(200);
+        }
+      });
   });
 });
